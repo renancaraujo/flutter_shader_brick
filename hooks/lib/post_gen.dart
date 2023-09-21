22b330bb95a22db6a46ff1f60156ea0fca2b11a0
Renan Araujo <renanmatheuscorreiaaraujo@gmail.com>
2023-09-21 01:12:34 +0100

    fix: improve pubspec validation

--- hooks/lib/post_gen.dart
+++ hooks/lib/post_gen.dart
@@ -1,6 +1,7 @@
 import 'dart:io' as io;
 import 'package:mason/mason.dart';
 import 'package:path/path.dart' as path;
+import 'package:pubspec_parse/pubspec_parse.dart';
 import 'package:yaml_edit/yaml_edit.dart';
 
 Future<void> postGen(HookContext context) async {
@@ -13,8 +14,29 @@ Future<void> postGen(HookContext context) async {
   final cwd = io.Directory.current;
   final pubspecFile = io.File(path.join(cwd.path, 'pubspec.yaml'));
   final yaml = pubspecFile.readAsStringSync();
-  final editor = YamlEditor(yaml)
-    ..appendToList(['flutter', 'shaders'], 'shaders/$snakeNamed.glsl');
+  final editor = YamlEditor(yaml);
+  final pubspec = Pubspec.parse(pubspecFile.readAsStringSync());
+  final shaderFileName = 'shaders/$snakeNamed.glsl';
+
+  final flutterEntry = pubspec.flutter;
+  if (flutterEntry == null) {
+    editor.update([
+      'flutter'
+    ], {
+      'shaders': [shaderFileName]
+    });
+  } else {
+    final shadersEntry = flutterEntry['shaders'];
+    if (shadersEntry == null) {
+      editor.update([
+        'flutter'
+      ], {
+        'shaders': [shaderFileName]
+      });
+    } else {
+      editor.appendToList(['flutter', 'shaders'], shaderFileName);
+    }
+  }
 
   pubspecFile.writeAsStringSync(editor.toString());
 
